ed79c7543e18ac33941ad7bebe262e4063ae7082
Elena Couto Fraga <elena.couto@rai.usc.es>
2024-09-30 12:28:18 +0200

    Agregadas funcionalidades para el triángulo

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from lib import cuadrado
+from lib import cuadrado, triangulo
 print("Proyecyo Figuras")
 print(cuadrado.get_identificador())
 lado=4
 print(f"El area de un cuadrado de lado {lado} es: 
       {cuadrado.get_area(lado)} y el perímetro es {cuadrado.get_perimetro(lado)}")
+
+base=4
+altura=2
+print(f"El área de un {triangulo.get_identificador()} de base {base} y altura {altura} es: 
+      {triangulo.get_area(base, altura)} y el perimetro es {triangulo.get_perimetro(base, altura)}")
